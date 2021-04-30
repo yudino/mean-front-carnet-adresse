4ca355f46655f02fab593ab6577862460505811e
yudino <yohane.udino@gmail.com>
2021-04-30 11:27:15 +0200

    Affichage contact : OK

--- src/app/friend-view/friend-view.component.ts
+++ src/app/friend-view/friend-view.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Schtroumpf} from '../models/Schtroumpf.model';
+import {Subscription} from 'rxjs';
+import {StateService} from '../services/state.service';
+import {CrudSchtroumpf} from '../services/crud-schtroumpf';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-friend-view',
   templateUrl: './friend-view.component.html',
   styleUrls: ['./friend-view.component.css']
 })
-export class FriendViewComponent implements OnInit {
+export class FriendViewComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public friend: Schtroumpf[] = [];
+  public loading: boolean;
+
+  private friendSub: Subscription;
+
+  constructor(private state: StateService,
+              private crudSchtroumpf: CrudSchtroumpf,
+              private router: Router) { }
+
 
   ngOnInit(): void {
+    this.loading = true;
+    this.state.mode$.next('list');
+    this.friendSub = this.crudSchtroumpf.friend$.subscribe(
+      (friend) => {
+        this.friend = friend;
+        this.loading = false;
+      }
+    );
+    this.crudSchtroumpf.getFriend();
+  }
+
+  onFriendClicked(id: string): void {
+    this.router.navigate(['/modify-friend/' + id]);
+  }
+
+  ngOnDestroy(): void {
+    this.friendSub.unsubscribe();
   }
 
 }
--- src/app/services/crud-schtroumpf.ts
+++ src/app/services/crud-schtroumpf.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Subject } from 'rxjs';
 import {Schtroumpf} from '../models/Schtroumpf.model';
 
 
@@ -9,7 +9,55 @@ import {Schtroumpf} from '../models/Schtroumpf.model';
 })
 
 export class CrudSchtroumpf {
-    constructor(private http: HttpClient) { }
+
+  constructor(private http: HttpClient) { }
+
+  private friend: Schtroumpf[] = [];
+  public friend$ = new Subject<Schtroumpf[]>();
+
+  getFriend() {
+    this.http.get('http://localhost:8080/api/carnet').subscribe(
+      (friend: Schtroumpf[]) => {
+        if (friend) {
+          this.friend = friend;
+          this.emitFriend();
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  emitFriend() {
+    this.friend$.next(this.friend);
+  }
+
+  getThingById(id: string) {
+    return new Promise((resolve, reject) => {
+      this.http.get('http://localhost:3000/api/stuff/' + id).subscribe(
+        (response) => {
+          resolve(response);
+        },
+        (error) => {
+          reject(error);
+        }
+      );
+    });
+  }
+
+  createNewFriend(schtroumpf: Schtroumpf) {
+    return new Promise((resolve, reject) => {
+      this.http.post('http://localhost:3000/api/carnet', schtroumpf).subscribe(
+        (response) => {
+          resolve(response);
+        },
+        (error) => {
+          reject(error);
+        }
+      );
+    });
+  }
 
   // tslint:disable-next-line:typedef
   createNewFriendWithFile(schtroumpf: Schtroumpf, image: File) {
@@ -24,6 +72,54 @@ export class CrudSchtroumpf {
         },
         (error) => {
           console.log('je suis dans error front');
+          reject(error);
+        }
+      );
+    });
+  }
+
+  modifyFriend(id: string, friend: Schtroumpf) {
+    return new Promise((resolve, reject) => {
+      this.http.put('http://localhost:8082/api/carnet/' + id, friend).subscribe(
+        (response) => {
+          resolve(response);
+        },
+        (error) => {
+          reject(error);
+        }
+      );
+    });
+  }
+
+  modifyFriendWithFile(id: string, friend: Schtroumpf, image: File | string) {
+    return new Promise((resolve, reject) => {
+      let friendData: Schtroumpf | FormData;
+      if (typeof image === 'string') {
+        friend.imageUrl = image;
+        friendData = friend;
+      } else {
+        friendData = new FormData();
+        friendData.append('thing', JSON.stringify(friend));
+        friendData.append('image', image, friend.race);
+      }
+      this.http.put('http://localhost:8080/api/carnet/' + id, friendData).subscribe(
+        (response) => {
+          resolve(response);
+        },
+        (error) => {
+          reject(error);
+        }
+      );
+    });
+  }
+
+  deleteFriend(id: string) {
+    return new Promise((resolve, reject) => {
+      this.http.delete('http://localhost:8080/api/carnet/' + id).subscribe(
+        (response) => {
+          resolve(response);
+        },
+        (error) => {
           reject(error);
         }
       );
